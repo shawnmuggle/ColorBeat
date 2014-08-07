b583e50ac3342c4974094966811041f65de171b2
Le Yu <ley@twitter.com>
2014-08-07 01:33:02 -0700

    Add Status

---
+++ ColorBeat/RoundedCornerView.h
@@ -0,0 +1,13 @@
+//
+//  RoundedCornerView.h
+//  ColorBeat
+//
+//  Created by Le Yu on 8/7/14.
+//  Copyright (c) 2014 Le Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoundedCornerView : UIView
+
+@end
---
+++ ColorBeat/RoundedCornerView.m
@@ -0,0 +1,40 @@
+//
+//  RoundedCornerView.m
+//  ColorBeat
+//
+//  Created by Le Yu on 8/7/14.
+//  Copyright (c) 2014 Le Yu. All rights reserved.
+//
+
+#import "RoundedCornerView.h"
+
+@implementation RoundedCornerView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (id)initWithCoder:(NSCoder *)aDecoder;
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
+- (void)commonInit;
+{
+    CALayer *layer = self.layer;
+    layer.cornerRadius  = 10.0f;
+    layer.masksToBounds = YES;
+}
+
+
+@end
