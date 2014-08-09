18f1bb5aa99ab6a9fa1163965bb2e10fcfe2d1dd
Le Yu <ley@twitter.com>
2014-08-08 22:48:17 -0700

    [MS] Inital Version

--- ColorBeat/HelpViewController.m
+++ ColorBeat/HelpViewController.m
@@ -9,41 +9,23 @@
 #import "HelpViewController.h"
 
 @interface HelpViewController ()
+@property (weak, nonatomic) IBOutlet UIView *imageView;
 
 @end
 
 @implementation HelpViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
+- (void) viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    UIImageView *imageSubView = [[UIImageView alloc] initWithFrame:self.imageView.bounds];
+    imageSubView.image = [UIImage imageNamed:@"screenshot2"];
+    [self.imageView addSubview:imageSubView];
+    
+    
 }
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)backGame:(id)sender {
+    [self.navigationController popViewControllerAnimated:TRUE];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
